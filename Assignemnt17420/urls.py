ae5937db9f30e5cf9673572d048753bcaab70e45
Jakeybaby <43561941+Jakeybaby@users.noreply.github.com>
2020-03-26 17:14:02 +1300

    Start Working on Assignment now

--- Assignemnt17420/urls.py
+++ Assignemnt17420/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+
+
+
 urlpatterns = [
-    path('app/', include('app.urls')),
+
+    path('',include('app.urls')),
     path('admin/', admin.site.urls),
+
+
 ]
--- app/urls.py
+++ app/urls.py
@@ -4,8 +4,6 @@ from . import views
 
 app_name = 'app'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('', views.homepage, name='homepage'),
+
 ]
