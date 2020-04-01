98254ff9e0ffea25e39676ba0911bb3a355b6a6a
Jakeybaby <43561941+Jakeybaby@users.noreply.github.com>
2020-04-01 22:30:38 +1300

    Register Funtion working

--- app/views.py
+++ app/views.py
@@ -1,29 +1,22 @@
-from audioop import reverse
+
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse, HttpResponseRedirect
-from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login,logout, authenticate
+from django.contrib.auth import login
+from . form import UserRegisterForm
 
 
 def home(request):
     return render(request, 'app/indexpage.html')
 
 def register(request):
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(user)
-            return redirect("app/demo.html")
-        else:
-            for msg in form.error_messages:
-                print(form.error_messages[msg])
+    form = UserRegisterForm()
 
-    form = UserCreationForm
-    return render(request,
-                  'app/register.html',
-                  context={"form":form})
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context = {'form':form}
+    return render(request, 'app/register.html',context)
 
 def demo(request):
     return render(request, 'app/demo.html')
